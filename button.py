44769c85a6363645dec7cc1a0b3ae315e5041f3d
alfarizamo <160070893+alfarizamo@users.noreply.github.com>
2024-09-09 17:34:13 +0700

    Create button.py

---
+++ button.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title('Fisika')
+st.header('Komputasi Awan')
+print('Holaa')
+st.button("Idihh", type="primary")
+if st.button("Woi"):
+    st.write("Pala Bapak Kau")
+else:
+    st.write("Jaa")
